754a30ea682b1d15aa34244b517793bfafcc0ae6
innocentmaweta-hub <innocentmaweta@gmail.com>
2026-09-11 15:19:56 +0200

    Add secure Electron preload bridge

---
+++ src/preload.js
@@ -0,0 +1,15 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('auto', {
+  startRecording: url => ipcRenderer.invoke('start-recording', url),
+  stopRecording: () => ipcRenderer.invoke('stop-recording'),
+  runWorkflow: repeats => ipcRenderer.invoke('run-workflow', repeats),
+  stopBrowser: () => ipcRenderer.invoke('stop-browser'),
+  getWorkflow: () => ipcRenderer.invoke('get-workflow'),
+  saveWorkflow: data => ipcRenderer.invoke('save-workflow', data),
+  on: (channel, callback) => {
+    const allowed = ['recorded-step', 'recording-state', 'run-status', 'browser-status', 'automation-error'];
+    if (!allowed.includes(channel)) return;
+    ipcRenderer.on(channel, (_, data) => callback(data));
+  }
+});
